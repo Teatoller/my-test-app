552d89da7521d092a9a6d9712d421e9bca0f1e02
Steven Ennis <steven.otieno1@gmail.com>
2021-05-12 11:31:40 +0300

    (userEvent): Implement with userEvent
    - add event test with userEvent
    
    [Starts #008]

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,5 @@
-import { render, screen, fireEvent, act } from '@testing-library/react';
+import { render, screen, fireEvent} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 describe('App', () => {
@@ -27,23 +28,43 @@ describe('App', () => {
     // screen.debug();
   });
 
-  describe('App', () => {
-    test('renders App component', async () => {
-      render(<App />);
-   
-      // wait for the user to resolve
-      // needs only be used in our special case
-      await screen.findByText(/Signed in as/);
-   
-      expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
-   
-      fireEvent.change(screen.getByRole('textbox'), {
-        target: { value: 'JavaScript' },
-      });
-   
-      expect(screen.getByText(/Searches for JavaScript/)).toBeInTheDocument();
-    });
-  });
+  
 
+});
+
+describe('App', () => {
+  test('renders App component', async () => {
+    render(<App />);
+ 
+    // wait for the user to resolve
+    // needs only be used in our special case
+    await screen.findByText(/Signed in as/);
+ 
+    expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
+ 
+    fireEvent.change(screen.getByRole('textbox'), {
+      target: { value: 'JavaScript' },
+    });
+ 
+    expect(screen.getByText(/Searches for JavaScript/)).toBeInTheDocument();
+  });
+});
+
+describe('App', () => {
+  test('renders App component', async () => {
+    render(<App />);
+ 
+    // wait for the user to resolve
+    await screen.findByText(/Signed in as/);
+ 
+    expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
+    // screen.debug();
+ 
+    await userEvent.type(screen.getByRole('textbox'), 'JavaScript');
+    // screen.debug();
+    expect(
+      screen.getByText(/Searches for JavaScript/)
+    ).toBeInTheDocument();
+  });
 });
 
