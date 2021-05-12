adc9cb71a1084f05fa088f4244d2ec6893e2c5f6
Steven Ennis <steven.otieno1@gmail.com>
2021-05-12 11:20:43 +0300

    (fire event): Implemnet fire event test
    - test search input event
    
    [Starts #007]

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, act } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
@@ -26,5 +26,24 @@ describe('App', () => {
     expect(await screen.findByText(/Signed in as/)).toBeInTheDocument();
     // screen.debug();
   });
+
+  describe('App', () => {
+    test('renders App component', async () => {
+      render(<App />);
+   
+      // wait for the user to resolve
+      // needs only be used in our special case
+      await screen.findByText(/Signed in as/);
+   
+      expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
+   
+      fireEvent.change(screen.getByRole('textbox'), {
+        target: { value: 'JavaScript' },
+      });
+   
+      expect(screen.getByText(/Searches for JavaScript/)).toBeInTheDocument();
+    });
+  });
+
 });
 
